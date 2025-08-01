da8f261d60f85812c2ee9a3ae36551049b7b28fc
Jay Michael Yetman <jayyetman@Jays-MBP-2.localdomain>
2025-08-01 09:53:33 -0400

    added user specific functionality

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@ import { generateClient } from "aws-amplify/data";
 const client = generateClient<Schema>();
 
 function App() {
-  const { signOut } = useAuthenticator();
+  // added user
+  const { user, signOut } = useAuthenticator();
   const navigate = useNavigate();
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
@@ -24,7 +25,9 @@ function App() {
 
   return (
     <AppWrapper>
-      <h1>My todos</h1>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
+      {/* <h1>My todos</h1> */}
+      
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
